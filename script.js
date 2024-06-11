54fbaaa8a3ebb489df7a5d1448c86e618101fc41
nazar7755 <nazarcorovol09@gmail.com>
2024-06-11 18:38:42 +0300

    Замена кода

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-"use strict";
+// const age = 10;
+// const firstName = "Nazar";
 
-const age = 10;
-const firstName = "Nazar";
+// console.log(age);
+// console.log(firstName);
 
-console.log(age);
-console.log(firstName);
+// const isVIP = true;
+// console.log(isVIP);
+
+// // const type = typeof 5;
+// // console.log(type);
+
+// console.log("This is a number", age);
+
+// console.log("before");
+// alert("Hello!");
+// console.log("after");
+
+// const shoudRenew = confirm("Хотите купить этот товар ?");
+// console.log(shoudRenew);
+
+// let userInput = prompt("Введите число 52");
+// userInput = Number(userInput);
+// console.log(typeof userInput);
+// console.log(userInput === 52);
