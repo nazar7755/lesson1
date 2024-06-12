0a10413dc1c17d1d20d289784a160c29f44567e8
nazar7755 <nazarcorovol09@gmail.com>
2024-06-12 14:27:39 +0300

    12.06.2024

--- script.js
+++ script.js
@@ -23,3 +23,70 @@
 // userInput = Number(userInput);
 // console.log(typeof userInput);
 // console.log(userInput === 52);
+
+// console.log("5" == 5);
+
+// const value = "qwerty";
+
+// const number = Number(value);
+
+// console.log(number);
+
+// const blockWidth = "300.5px";
+// const width = Number.parseFloat(blockWidth);
+
+// console.log(typeof width);
+
+// let number = prompt("Введите число");
+// number = Number(number);
+// let power = prompt("Введите степень ");
+// power = Number(power);
+// console.log(number);
+// console.log(power);
+
+// const result = Math.pow(number, power);
+// console.log(result);
+
+// const firstName = "Nazar";
+// const lastName = "Chornovol";
+// const room = "15";
+// const type = "VIP";
+
+// const fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+// const message =
+//   "Гость " +
+//   firstName +
+//   " " +
+//   lastName +
+//   " " +
+//   "поселяется в " +
+//   type +
+//   " номер " +
+//   room;
+
+// const message = `Гость ${firstName} ${lastName} поселяется в ${type} номер ${room}`;
+
+// console.log(message);
+
+// const value = "QWERTY";
+
+// const input = prompt("Что хочешь");
+
+// console.log("input before", input);
+// console.log("input after", input);
+// const normalizInput = input.toLowerCase();
+// console.log("normalizInput: ", normalizInput);
+// console.log(value === normalizInput);
+
+const number = 60;
+
+// const isInRange = number > 10 && number < 30;
+
+// console.log(`Число ${number} попадает в отрезок от 10 до 30`, isInRange);
+
+const isInRange = number < 10 || number > 30;
+
+console.log(`Число ${number} попадает в отрезок от 10 до 30`, isInRange);
